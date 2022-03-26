8c697edee1cfff5b4f60a72b50d63a7d792d3ba8
Alisson-developer <alissonlua7@gmail.com>
2022-03-26 14:26:38 -0300

    Primeiro commit pro github

---
+++ src/components/About/index.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+
+export default function about() {
+    return (
+        <div className='about-cntainer'>
+            <h1>Sobre mim!</h1>
+            <h2>Fullstack Developer at Radix</h2>
+            
+            <p>Autodidata em programação e amante do desenvolvimento fullstack, busco sempre me atualizar e aprimorar meus conhecimentos para alcançar meu objetivo de carreira como Arquiteto de Software. Atualmente estou focado em aprender padrões arquiteturais, técnicas de algoritmos e análise de complexidade do mesmo. As tecnologias que utilizo são, Java, Spring, ReactJS e React Native.</p>
+        </div>
+    )
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+
+
+export default function header() {
+    return (
+        <header>
+            <div className='img-container'>
+                <div className='img-profile' />
+            </div>
+        </header>
+    )
+}
