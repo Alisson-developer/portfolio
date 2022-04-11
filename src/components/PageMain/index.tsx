cdcaebb61592388ce7ee514211e3188c10c58f95
Alisson-developer <alissonlua7@gmail.com>
2022-04-11 00:48:54 -0300

    Adicionada a seção de fomulário de email

--- src/components/PageMain/index.tsx
+++ src/components/PageMain/index.tsx
@@ -1,11 +1,13 @@
 import './index.css'
 import Slide from '../Slide'
+import Email from '../Email'
 
 export default function PageMain(props: any) {
 
     return (
         <div className={props.visible === true ? 'pageBodyVisible' : 'pageBodyEmpty'}>
             <Slide visible={props.visible} />
+            <Email visible={props.visible} />
         </div>
     )
 }
